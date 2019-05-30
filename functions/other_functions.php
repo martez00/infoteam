f10659f319372bf86de4f579a4d6f1c972e85e0b
martynas.sebestinas <martynas@infotrans.lt>
2019-05-30 17:43:45 +0300

    Pridėtas favicon

--- functions/other_functions.php
+++ functions/other_functions.php
@@ -364,7 +364,7 @@ function return_actions_table($items, $kiek_viso_irasu, $limit_key, $page)
                 else if ($item['table_name'] == 'applications_to_club') $href = "<a href='$GLOBALS[url_path]applications/edit_application.php?id=$item[record_id]' target='_blank'>$item[record_name]</a>";
                 else if ($item['table_name'] == 'users') $href = "<a href='$GLOBALS[url_path]users/user.php?id=$item[record_id]' target='_blank'>$item[record_name]</a>";
             }
-            else $href = "ID: $item[record_id]";
+            else $href = "ID: $item[record_id] <small>(nebeegzistuoja)</small>";
             $text .= "<tr>
 <td>" . $item['id'] . "</td>
 <td>" . $href . "</td>
--- system/inc/head.inc.php
+++ system/inc/head.inc.php
@@ -3,7 +3,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-
+    <link rel="shortcut icon" href="<?php echo $GLOBALS['url_path']; ?>favicon.png" type="image/x-icon"/>
 
     <!-- Bootstrap core CSS-->
     <link href="<?php echo $GLOBALS['url_path']; ?>vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
